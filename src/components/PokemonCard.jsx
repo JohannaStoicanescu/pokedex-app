381fd1bc9b2666c28c9918233a9fa42e9d24ad02
Johanna <johanna.stoicanescu@gmail.com>
2023-09-20 11:38:32 +0200

    a little bit of style

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,13 @@
+import styles from "./PokemonCard.module.css";
+
 export function PokemonCard() {
   return (
     <>
-      <figure>
+      <figure className={styles.card}>
         <img
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
           alt="Bulbasaur"
+          className={styles.cardImg}
         />
         <figcaption>Bulbasaur</figcaption>
       </figure>
