cc363ef611058c644ac57a4cb7caed0ebf3a5d08
Johanna <johanna.stoicanescu@gmail.com>
2023-09-20 14:14:39 +0200

    Pokédex 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,30 @@
 import styles from "./PokemonCard.module.css";
 
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export function PokemonCard() {
+  const pokemon = pokemonList[0];
+  //const pokemon = pokemonList[1];
+
   return (
     <>
       <figure className={styles.card}>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasaur"
-          className={styles.cardImg}
-        />
+        {pokemon.imgSrc ? (
+          <img
+            src={pokemon.imgSrc}
+            alt={pokemon.name}
+            className={styles.cardImg}
+          />
+        ) : null}
         <figcaption>Bulbasaur</figcaption>
       </figure>
     </>
